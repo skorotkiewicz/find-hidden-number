9297cc9e3f0d3480cab2d9f8edff68e7e3badee6
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2022-08-29 00:20:04 +0200

    add service worker for offline

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/find-hidden-number/",
-  plugins: [react()],
+  plugins: [VitePWA({ registerType: "autoUpdate" }), react()],
 });
